5466c325afea0b4a6113e2844c9b1ffa421e7831
ivan-maltcev <skirym2199@gmail.com>
2025-03-31 19:09:37 +0300

    Выполнено задание 3

--- hw-3.js
+++ hw-3.js
@@ -1,26 +1,6 @@
-let a = 0;
-if (a > 0 && a < 10) {
-    alert('Верно');
-} else {
-    alert('Неверно');
-}
-
-let b = 10;
-if (b > 0 && b < 10) {
-    alert('Верно');
-} else {
-    alert('Неверно');
-}
-
-let c = -3;
-if (c > 0 && c < 10) {
-    alert('Верно');
-} else {
-    alert('Неверно');
-}
-
-let d = 2;
-if (d > 0 && d < 10) {
+let d = 50;
+let e = 150;
+if (d > 100 || e > 100) {
     alert('Верно');
 } else {
     alert('Неверно');
