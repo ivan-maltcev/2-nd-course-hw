1454ccf73a66f5d9d20a92e647523dfdad7aa7bd
ivan-maltcev <skirym2199@gmail.com>
2025-03-31 18:51:11 +0300

    Сделал задание 2

--- hw-3.js
+++ hw-3.js
@@ -1,7 +1,27 @@
-let password = 'пароль';
-let userInput = prompt('Введите пароль');
-if (userInput === password) {
-    alert('Введен верный пароль');
+let a = 0;
+if (a > 0 && a < 10) {
+    alert('Верно');
 } else {
-    alert('Пароль введен неверно')
+    alert('Неверно');
+}
+
+let b = 10;
+if (b > 0 && b < 10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+let c = -3;
+if (c > 0 && c < 10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+let d = 2;
+if (d > 0 && d < 10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
 }
