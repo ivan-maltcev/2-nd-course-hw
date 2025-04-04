5b53c1a878b9050ce338bcb777fc2175991f241e
ivan-maltcev <skirym2199@gmail.com>
2025-04-04 13:46:26 +0300

    добавлены недостающие задачи

--- hw-3.js
+++ hw-3.js
@@ -1,3 +1,51 @@
+let password = 'привет';
+let userInput = prompt('Введите пароль');
+if (userInput === password) {
+    alert('Пароль введен верно')
+} else {
+    alert('Пароль введен неверно!')
+}
+
+let a = 0;
+if (a > 0 && a < 10) {
+    alert('Верно')
+} else {
+    alert('Неверно')
+}
+
+let b = 10;
+ if (b > 0 && b < 10) {
+     alert('Верно');
+ } else {
+     alert('Неверно');
+ }
+
+ let c = -3;
+ if (c > 0 && c < 10) {
+     alert('Верно');
+ } else {
+     alert('Неверно');
+ }
+
+ let d = 2;
+ if (d > 0 && d < 10) {
+     alert('Верно');
+ } else {
+     alert('Неверно');
+ }
+
+ let e = 50;
+ let f = 150;
+ if (e > 100 || f > 100) {
+     alert('Верно');
+ } else {
+     alert('Неверно');
+ }
+
+ let g = '2';
+ let h = '3';
+ alert(Number(g) + Number(h));
+
 let monthNumber = Number(prompt('Введите номер месяца (от 1 до 12):'));
 if (isNaN(monthNumber)) {
     alert('Это не число!');
